f60308661e1041401ed09c6605cc390afa052600
Mudit Garg <muditgarg48@gmail.com>
2024-03-03 23:44:23 +0000

    Capitalising global variable in tensorboard training monitor script

--- tensorboard_training_monitor.py
+++ tensorboard_training_monitor.py
@@ -1,9 +1,9 @@
 import os
-from global_variables import logger_folder_name
+from global_variables import LOGGER_FOLDER_NAME
 
 def launch_tensorboard_monitor():
     # os.system('ls' if os.name == 'nt' else 'dir')
-    os.system(f'cd {logger_folder_name}')
+    os.system(f'cd {LOGGER_FOLDER_NAME}')
     os.system('cd train')
     os.system('tensorboard --logdir=.')
 
