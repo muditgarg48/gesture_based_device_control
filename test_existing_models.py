71c078299193e315c1c3f6229ae0d4277f044be0
Mudit Garg <muditgarg48@gmail.com>
2024-03-05 19:23:54 +0000

    Flipping the camera feed in x-axis in testing a model script

--- test_existing_models.py
+++ test_existing_models.py
@@ -32,7 +32,7 @@ def prob_viz(res, actions, input_frame, colors):
     output_frame = input_frame.copy()
     for num, prob in enumerate(res):
         cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
-        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA)
     return output_frame
 
 def test_from_camera_feed(model, actions):
@@ -77,6 +77,8 @@ def test_from_camera_feed(model, actions):
             keypoints = extract_keypoints(results)
             sequence.append(keypoints)
             sequence = sequence[-30:]
+
+            image = cv2.flip(image, 1)
             
             if len(sequence) == 30:
                 res = model.predict(np.expand_dims(sequence, axis=0))[0]
@@ -101,12 +103,12 @@ def test_from_camera_feed(model, actions):
 
                 # Viz probabilities
                 image = prob_viz(res, actions, image, colors)
-                
+
             cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
             cv2.putText(image, ' '.join(history), (3,30), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
             cv2.putText(image, 'Current command: ' + current_command, (3,240), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
             
             # Show to screen
             cv2.imshow(WINDOW_NAME, image)
