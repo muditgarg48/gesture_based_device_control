f3b75c26fa94a356e8a38bfbff293fc060c2fe30
ananyagarg22 <67791767+ananyagarg22@users.noreply.github.com>
2024-03-21 04:37:15 +0000

    Adding a section in the website to add accept a scenario's details for further processing (creation of new folder to store training data and gestures and trained model)

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import subprocess
 
 app = Flask(__name__)
+
+from scripts.commandline_functions import run_command
+result_code = run_command(["python3 --version"])
+if result_code == 0:
+    python_command = 'python3'
+else:
+    python_command = 'python'
 
 @app.route('/')
 def index():
@@ -9,7 +16,7 @@ def index():
 
 @app.route('/run_project_setup')
 def run_project_setup():
-    process = subprocess.Popen(['python', 'project_setup.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen([python_command, 'project_setup.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = process.communicate()
     if error:
         return f"An error occurred: {error.decode()}"
@@ -18,7 +25,7 @@ def run_project_setup():
     
 @app.route('/run_integrity_check')
 def run_integrity_check():
-    process = subprocess.Popen(['python', 'project_integrity_check.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen([python_command, 'project_integrity_check.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = process.communicate()
     if error:
         return f"An error occurred: {error.decode()}"
@@ -34,6 +41,14 @@ def run_camera_check():
         return "Camera feed accessibility: " + success()
     else:
         return "Camera feed accessibility: " + failure()
+
+@app.route('/create_new_scenario', methods=['POST'])
+def create_new_scenario():
+    scenario_name = request.form['scenario_name']
+    scenario_gestures = request.form['scenario_gestures'].split('\r\n')
+    print(scenario_name)
+    print(scenario_gestures)
+    return "Scenario name and gestures received. Go back to collect training data for this scenario and train an LSTM Neural Network for the same."
 
 if __name__ == '__main__':
     app.run(debug=True)
